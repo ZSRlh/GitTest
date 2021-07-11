55a20cd370268691fe4f4f7a907daf8d29331c91
zsrilh <952177238@qq.com>
2021-07-11 17:50:31 +0800

    zsr init

--- test.js
+++ test.js
@@ -1,13 +1,1 @@
-console.log("test")
-console.log("debug")
-console.log("nobug")
-
-
-console.log('object')
-
-console.log('Master update')
-// fix
-
-console.log('dev update')
-
-console.log('master update again')
+console.log('zsrlh')
